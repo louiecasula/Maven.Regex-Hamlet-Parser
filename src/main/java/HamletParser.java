77ac575074af6ca5ab71f351a8eefa4cc49d8e4a
louie <louiecasula@gmail.com>
2023-12-06 08:58:11 -0500

    Wrote tests, made logic, passed tests

--- src/main/java/HamletParser.java
+++ src/main/java/HamletParser.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by thook on 10/7/15.
@@ -36,4 +38,53 @@ public class HamletParser {
         return hamletData;
     }
 
+    public String changeHamletToLeon(String input) {
+        Pattern pattern = Pattern.compile("Hamlet");
+        Matcher matcher = pattern.matcher(input);
+        input = matcher.replaceAll("Leon");
+        pattern = Pattern.compile("HAMLET");
+        matcher = pattern.matcher(input);
+        input = matcher.replaceAll("LEON");
+        return input;
+    }
+
+    public String changeHoratioToTariq(String input) {
+        Pattern pattern = Pattern.compile("Horatio");
+        Matcher matcher = pattern.matcher(input);
+        input = matcher.replaceAll("Tariq");
+        pattern = Pattern.compile("HORATIO");
+        matcher = pattern.matcher(input);
+        input = matcher.replaceAll("TARIQ");
+        return input;
+    }
+
+    public int findHamlet(String input) {
+        int count = 0;
+        Pattern pattern = Pattern.compile("Hamlet");
+        Matcher matcher = pattern.matcher(input);
+        while(matcher.find()){
+            count++;
+        }
+        pattern = Pattern.compile("HAMLET");
+        matcher = pattern.matcher(input);
+        while(matcher.find()){
+            count++;
+        }
+        return count;
+    }
+
+    public int findHoratio(String input) {
+        int count = 0;
+        Pattern pattern = Pattern.compile("Horatio");
+        Matcher matcher = pattern.matcher(input);
+        while(matcher.find()){
+            count++;
+        }
+        pattern = Pattern.compile("HORATIO");
+        matcher = pattern.matcher(input);
+        while(matcher.find()){
+            count++;
+        }
+        return count;
+    }
 }
--- src/test/java/HamletParserTest.java
+++ src/test/java/HamletParserTest.java
@@ -1,5 +1,9 @@
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.junit.Assert.*;
 
@@ -15,17 +19,26 @@ public class HamletParserTest {
 
     @Test
     public void testChangeHamletToLeon() {
+        String output = hamletParser.changeHamletToLeon(hamletText);
+        Assert.assertTrue(hamletParser.findHamlet(output) == 0);
+        System.out.println(output);
     }
 
     @Test
     public void testChangeHoratioToTariq() {
+        String output = hamletParser.changeHoratioToTariq(hamletText);
+        Assert.assertTrue(hamletParser.findHoratio(output) == 0);
     }
 
     @Test
     public void testFindHoratio() {
+        String horatioExample = "Horatio. Horatio? Horatio: Horatio! Horatio's Horatio, HORATIO HORATIO:";
+        Assert.assertEquals(8, hamletParser.findHoratio(horatioExample));
     }
 
     @Test
     public void testFindHamlet() {
+        String hamletExample = "Hamlet. Hamlet? Hamlet: Hamlet! Hamlet's Hamlet, HAMLET HAMLET:";
+        Assert.assertEquals(8, hamletParser.findHamlet(hamletExample));
     }
 }
